1994e4d808e85499d83314e9e58b2b91de90749b
Nathaniel Volfango <nvolfango@gmail.com>
2018-11-04 15:24:37 +0000

    Changed fn naming convention and stockReturns variable

--- src/optimiser.py
+++ src/optimiser.py
@@ -5,9 +5,9 @@ import math as m
 
 # Calculates the mean, variance and standard deviation of a list of values
 # given by the 'prices' argument.
-def getMeanAndVolReturns(prices):
+def get_mean_and_volatility(prices):
     priceArr = np.array(prices)
-    stockReturns = priceArr[1:len(prices)] - priceArr[0:len(prices)-1]
+    stockReturns = (priceArr[1:len(prices)] - priceArr[0:len(prices)-1]) / priceArr[0:len(prices)-1]
 
     mean = np.mean(stockReturns)
     sumSq = 0
@@ -33,7 +33,7 @@ means = {}
 variances = {}
 stdDevs = {}
 for symbol in data:
-    means[symbol], variances[symbol], stdDevs[symbol] = getMeanAndVolReturns(data[symbol])
+    means[symbol], variances[symbol], stdDevs[symbol] = get_mean_and_volatility(data[symbol])
 
 # Combine inputs into one (dictionary)
 derivedData = {}
