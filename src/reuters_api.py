979596a16833c9e0711ced5870e0d7846a3c0e11
Ronan Prendiville <prendivilleronan@gmail.com>
2018-11-17 14:46:54 +0000

    Alter P/E Acquisition Code

--- src/reuters_api.py
+++ src/reuters_api.py
@@ -1,20 +1,18 @@
 import requests
-from bs4 import BeautifulSoup
-import re
+import pandas as pd
+import yahoo_api as yapi
 
-cookie = "AQIC5wM2LY4Sfcyvehmk6JH4q3hXRkRWNeTeLiytQM6PeVY%3D%40AAJTSQACMTAAAlNLABQtODIyODM0Nzg5OTI4NDg4MTU3OQACUzEAAjI3%23"
+cookie = "AQIC5wM2LY4SfczmIvCB0s3thxuTx5pbrX9wvcJ4fAwN8c8%3D%40AAJTSQACMTAAAlNLABQtMjUxMTUxMzgyMTk3MDg1NTY2MgACUzEAAjI4%23"
 headers = {"Cookie": "iPlanetDirectoryPro=" + cookie + ";Path=/"}
 tickers = ["MSFT", "AAPL", "AMZN"]
 
+pe_dict = {}
 
-for stock in tickers:
-    r = requests.get("https://emea1.apps.cp.thomsonreuters.com/Explorer/EVzCORPxFUNDTALSzRATIO.aspx?taxonomy=global&s=" + stock + ".O&st=RIC&mode=full-topMenu-banner-tabBar-leftMenu-relNav&tempcss=schema4&embeddedView=true&trace=", headers=headers)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    div = soup.find("div", id="EVzCORPxFUNDTALSzRATIO")
-    table = div.find("table")
-    td = table.find_all('td')[3:4]
-    print(td[0])
-    table2 = td.find("table")
-    tr = table2.find("tr", class_="dr row-o")
-    span= tr.find("span", class_="profitable")
-    print(span)
+tickers_by_sector = yapi.s_and_p_500_tickers_by_sector()
+for industry in tickers_by_sector:
+    for stock in tickers_by_sector[industry]:
+        r = requests.get("https://emea1.apps.cp.thomsonreuters.com/Explorer/EVzCORPxFUNDTALSzRATIO.aspx?taxonomy=global&s=" + stock + ".O&st=RIC&mode=full-topMenu-banner-tabBar-leftMenu-relNav&tempcss=schema4&embeddedView=true&trace=", headers=headers)
+        tables = pd.read_html(r.text, header=0, index_col=0)
+        pe = tables[7].loc["Curr P/E Excl Extra, LTM:", "LTM"]
+        pe_dict[stock] = pe
+print(pe_dict)
