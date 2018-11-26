930afaf506454d308a1a062a0984a76659272630
Nathaniel Volfango <nvolfango@gmail.com>
2018-11-26 19:06:08 +0000

    Optimiser test

--- src/test.py
+++ src/test.py
@@ -81,69 +81,150 @@
 # stocks = ["JNJ", "ABBV", "HD", "DIS"]
 # start = datetime.datetime(2017, 2, 11)
 # end = datetime.datetime(2018, 2, 11)
+# #
+# # data = yahoo.get_price_dataframe(stocks, start, end)
+# # print(data.head())
+# #
+# # engine = create_engine('postgresql://'+username+':'+password+'@'+hostname+':'+port+'/'+database)
+# # # writing to database
+# # data.to_sql(name='data_table', con=engine, if_exists='replace', index=True)
+# # # reading from database
+# # data_again = pd.read_sql('SELECT * from data_table', engine)
+# # # trim out time component from Date column
+# # data_again['Date'] = data_again['Date'].dt.date
+# #
+# # print(data_again.head())
 #
-# data = yahoo.get_price_dataframe(stocks, start, end)
-# print(data.head())
 #
-# engine = create_engine('postgresql://'+username+':'+password+'@'+hostname+':'+port+'/'+database)
-# # writing to database
-# data.to_sql(name='data_table', con=engine, if_exists='replace', index=True)
-# # reading from database
-# data_again = pd.read_sql('SELECT * from data_table', engine)
-# # trim out time component from Date column
-# data_again['Date'] = data_again['Date'].dt.date
+# # Example for DB operations, create, append, fetch and delete
+# import yahoo_api as yahoo
+# import datetime
+# from db_engine import DbEngine
+# import pandas as pd
 #
-# print(data_again.head())
+# db = DbEngine()
+# # stocks = ["JNJ", "ABBV", "HD", "DIS"]
+# # start = datetime.datetime(2017, 2, 11)
+# # end = datetime.datetime(2018, 2, 11)
+# # data = yahoo.get_price_dataframe(stocks, start, end)
+# # db.create_db_dataframe(data,'test_df1')
+# # data_again = db.fetch_db_dataframe('closing_prices_s_and_p')
+# # print(data_again)
+#
+# # db.append_db_dataframe(data, 'test_df1')
+# # data_again1 = db.fetch_db_dataframe('test_df1')
+# # print(data_again1)
+#
+#
+# # ranked_stocks_df = db.fetch_db_dataframe("rankings_table")
+# # # print(ranked_stocks_df)
+# # print(ranked_stocks_df.nsmallest(30, "Ranking_Score"))
+#
+# ranked_scores = db.fetch_db_dataframe("rankings_table")
+# sectors = db.fetch_db_dataframe("sector_table")
+# prices = db.fetch_db_dataframe("closing_prices_s_and_p")
+#
+# top30 = ranked_scores.nsmallest(30, "Ranking_Score")
+#
+# top30prices = pd.DataFrame(prices["Date"])
+# # for stock in top30["Ticker"]:
+# #     top30temp = prices[stock]
+# #     # print(top30temp)
+# #     top30prices.join(top30temp)
+# #     print(top30prices.join(top30temp))
+# # print(len(top30.index))
+#
+# ranked_scores = db.fetch_db_dataframe("rankings_table")
+# top_30_ranking_scores = ranked_scores.nsmallest(30, "Ranking_Score")
+# stocks = []
+# for stock in top_30_ranking_scores["Ticker"]:
+#     stocks.append(stock)
+# print(stocks)
+#
+#
+#
+#
+#
+#
 
-
-# Example for DB operations, create, append, fetch and delete
-import yahoo_api as yahoo
+import pandas_datareader as pdr
+import pandas as pd
 import datetime
 from db_engine import DbEngine
-import pandas as pd
-
-db = DbEngine()
-# stocks = ["JNJ", "ABBV", "HD", "DIS"]
-# start = datetime.datetime(2017, 2, 11)
-# end = datetime.datetime(2018, 2, 11)
-# data = yahoo.get_price_dataframe(stocks, start, end)
-# db.create_db_dataframe(data,'test_df1')
-# data_again = db.fetch_db_dataframe('closing_prices_s_and_p')
-# print(data_again)
-
-# db.append_db_dataframe(data, 'test_df1')
-# data_again1 = db.fetch_db_dataframe('test_df1')
-# print(data_again1)
 
 
-# ranked_stocks_df = db.fetch_db_dataframe("rankings_table")
-# # print(ranked_stocks_df)
-# print(ranked_stocks_df.nsmallest(30, "Ranking_Score"))
-
-ranked_scores = db.fetch_db_dataframe("rankings_table")
-sectors = db.fetch_db_dataframe("sector_table")
-prices = db.fetch_db_dataframe("closing_prices_s_and_p")
-
-top30 = ranked_scores.nsmallest(30, "Ranking_Score")
-
-top30prices = pd.DataFrame(prices["Date"])
-# for stock in top30["Ticker"]:
-#     top30temp = prices[stock]
-#     # print(top30temp)
-#     top30prices.join(top30temp)
-#     print(top30prices.join(top30temp))
-# print(len(top30.index))
-
-ranked_scores = db.fetch_db_dataframe("rankings_table")
-top_30_ranking_scores = ranked_scores.nsmallest(30, "Ranking_Score")
-stocks = []
-for stock in top_30_ranking_scores["Ticker"]:
-    stocks.append(stock)
-print(stocks)
+def get_adj_closing_prices(ticker, start=None, end=None, data_source="yahoo"):
+    """Returns a pandas series of adjusted closing prices for a given ticker
+    Parameters:
+    ticker: The ticker of the company or index to retrieve closing prices from
+    start: Start date as either a "yyyy-mm-dd" string or a datetime object. Defaults to one year before the current date
+    end: end date as either a "yyyy-mm-dd" string or a datetime object. Defaults to most recent closing price if left blank
+    data_source: data source
+    """
+    if start is None:
+        start = datetime.datetime.now() - datetime.timedelta(days=365)
+    df = pdr.DataReader(ticker, data_source=data_source, start=start, end=end)
+    return df["Adj Close"]
 
 
+def s_and_p_500_tickers_by_sector():
+    """Returns a dictionary of sectors with the corresponding tickers of all the companies in the S&P500
+    """
+    sectors_tickers={} #dictionary
+    wikipedia_url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    df = pd.read_html(wikipedia_url,header=0)[0]
+    for i in df['GICS Sector'].unique():
+        sectors_tickers[i]=[df['Symbol'][j] for j in df[df['GICS Sector']==i].index]
+    return sectors_tickers
+
+def store_data_csv(tickers, start, end):
+    """Returns a csv file containing adjusted closing prices for a range of tickers.
+    tickers = list of indexes of companies
+    """
+
+    closing_prices = pd.DataFrame()
+
+    for i in tickers:
+        closing_prices[i] = get_adj_closing_prices(i, start, end)
+    closing_prices.to_csv("storedata.csv")
 
 
+def get_price_dataframe(tickers, start, end):
+    """Same function as above however it returns a dataframe
+    containing adjusted closing prices for a range of tickers """
+    closing_prices = pd.DataFrame()
+    for i in tickers:
+        closing_prices[i] = get_adj_closing_prices(i, start, end)
+    return closing_prices
 
 
+if __name__ == "__main__":
+    #The statement if __name__ == "__main__" checks if this file was imported or
+    #run directly. If this file was imported, __name__ == "__main__" evaluates to false.
 
+    #The direct result of this is that importing this module using "import yahoo_api" will
+    #not cause the test code to run.
+
+    # Examples of how to use functions
+    # df = get_price_dataframe(["WELL", "AAPL", "WFC", "WDC", "GOOG"], "2017-11-02", "2018-11-02")
+    # print(df)
+    # store_data_csv(["WELL", "AAPL", "WFC", "WDC", "GOOG"], "2017-11-02", "2018-11-02")
+
+    #created table in db containing price data for the 500 stocks
+    db = DbEngine()
+
+    # db.delete_table('closing_prices_s_and_p')
+    sectors_tickers= s_and_p_500_tickers_by_sector()
+    # stocks = ['AAPL', 'AMZN', 'GOOG']
+    stocks = [stock for sublist in sectors_tickers.values() for stock in sublist]
+    try:
+        data = get_price_dataframe(stocks, "2017-11-23", "2018-11-23")
+    except:
+        data = get_price_dataframe(stocks, "2017-11-23", "2018-11-23")
+    print(data)
+    db.create_db_dataframe(data,'closing_prices_s_and_p')
+    data_again = db.fetch_db_dataframe('closing_prices_s_and_p')
+    print(data_again)
+    db.append_db_dataframe(data, 'closing_prices_s_and_p')
+    data_again1 = db.fetch_db_dataframe('closing_prices_s_and_p')
+    print(data_again1)
