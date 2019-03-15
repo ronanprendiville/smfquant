86c2cb93cb10eda8c85d3512c898728143cac13c
tao-yu <taoxyu@gmail.com>
2019-03-15 16:37:19 +0000

    object oriented scipy optimiser + grid search.

--- src/scipy_optimizer.py
+++ src/scipy_optimizer.py
@@ -3,18 +3,21 @@ from db_engine import DbEngine
 import numpy as np
 import time
 import pandas as pd
+from itertools import product
 
-new_portfolio_allocation = 60000
-risk_free_rate = 0
-bounds = (0, 1) # All weights will be in this range
-penalty = 0 # The higher this is, the fewer nontrivial weights there will be
 
-def calculate_optimiser_inputs(tickers):
+def calculate_optimiser_inputs(db,
+                               num_stocks,
+                               rankings_table,
+                               prices_table):
     """Returns two pandas DataFrames: mean (daily) returns for each
      stock in tickers and matrix of covariances between stocks' returns."""
 
-    prices = db.fetch_db_dataframe("closing_prices_s_and_p_3")
-    prices_of_top_30 = prices.loc[:,tickers]
+    prices = db.fetch_db_dataframe(prices_table)
+    ranked_scores = db.fetch_db_dataframe(rankings_table)
+    top_ranking_scores = ranked_scores.nsmallest(num_stocks, "Ranking_Score")
+    stocks = list(top_ranking_scores['Ticker'])
+    prices_of_top_30 = prices[stocks]
 
     # Gets the mean of (daily) historical log returns and the covariance of stock log returns.
     log_returns = np.log(prices_of_top_30).diff()
@@ -22,61 +25,108 @@ def calculate_optimiser_inputs(tickers):
     covariances = log_returns.cov()
     return mean, covariances
 
-def neg_sharpe(weights, penalty=0):
-    portfolio_sd = np.sqrt(weights @ covariance_of_returns.values @ weights * 252)
-    portfolio_ev = weights @ mean_historical_returns.values * 252
+
+def neg_sharpe(weights, mu, Sigma,
+               penalty=0, risk_free_rate=0):
+    portfolio_sd = np.sqrt(weights @ Sigma.values @ weights * 252)
+    portfolio_ev = weights @ mu.values * 252
     sharpe = (portfolio_ev - risk_free_rate)/portfolio_sd
     return -sharpe + penalty * np.sum(weights**2)
 
-def optimise_portfolio_slsqp(mean_historical_returns, 
-                            covariance_of_returns, 
-                            risk_free_rate=0, 
-                            bounds=(0, 1),
-                            penalty=0):
-    res = sco.minimize(
-        neg_sharpe,
-        args=(penalty),
-        x0=np.random.uniform(size=30),
-        method="SLSQP",
-        bounds=[bounds] * 30,
-        constraints=[{"type": "eq", "fun": lambda x: np.sum(x) - 1}]
-    )
-    if np.abs(np.sum(res.x) - 1) > 0.0001:
-        print("=" * 50 + "\nWARNING: portfolio weights do not sum to 1\n" + "=" * 50)
-    return res.x * new_portfolio_allocation
+
+class Optimizer:
+    def __init__(self, mu, Sigma, bounds=(0, 1),
+                 penalty=0, risk_free_rate=0, warnings=False):
+        self._mu = mu
+        self._Sigma = Sigma
+        self._bounds = bounds
+        self._penalty = penalty
+        self._risk_free_rate = risk_free_rate
+        num_stocks = len(mu)
+
+        res = sco.minimize(
+            neg_sharpe,
+            args=(mu, Sigma, penalty, risk_free_rate),
+            x0=np.random.uniform(size=num_stocks),
+            method="SLSQP",
+            bounds=[bounds] * num_stocks,
+            constraints=[{"type": "eq", "fun": lambda x: np.sum(x) - 1}]
+        )
+
+        if warnings and np.abs(np.sum(res.x) - 1) > 0.0001:
+            print("WARNING: Weights do not sum up to 1")
+
+        self._weights = res.x
+
+    @property
+    def weights(self):
+        return self._weights
+
+    @property
+    def bounds(self):
+        return self._bounds
+    
+    @property
+    def penalty(self):
+        return self._penalty
+
+    @property
+    def mean(self):
+        return self._weights @ self._mu.values * 252
+    
+    @property
+    def sd(self):
+        return np.sqrt(self._weights @ self._Sigma.values @ self._weights * 252)
+
+    @property
+    def sharpe(self):
+        return (self.mean - self._risk_free_rate) / self.sd
+
+    def print_summary(self):
+        print(50 * "=")
+        if np.abs(np.sum(self._weights) - 1) > 0.001:
+            print(f"Weights add up to {np.sum(self._weights)}")
+        print(f"sharpe: {self.sharpe:.4f} mean: {self.mean:.4f} sd: \
+{self.sd:.4f} bounds: {self.bounds} penalty {self.penalty}")
+        percent_weights = np.round(100 * self._weights, 2)
+        print(sorted(percent_weights, reverse=True))
+
+    def __str__(self):
+        percent_weights = np.round(100 * self._weights, 4)
+        return " ".join(str(x) for x in (sorted(percent_weights, reverse=True)))
+
 
 if __name__ == "__main__":
-    # Initialise Database Engine
-
-
-    start = time.time()
-
     db = DbEngine()
+    mu, Sigma = calculate_optimiser_inputs(
+        db,
+        30,
+        "rankings_table_3",
+        "closing_prices_s_and_p_3"
+    )
 
-    ranked_scores = db.fetch_db_dataframe("rankings_table_3")
-    top_ranking_scores = ranked_scores.nsmallest(30, "Ranking_Score")
-    stocks = [stock for stock in top_ranking_scores["Ticker"]]
+    #o = Optimizer(mu, Sigma, (0.01, 1), 26)
+    #print(o.weights)
+    #o.print_summary()
+    #weights = o.weights
+    #print(np.count_nonzero(weights == weights.min()))
+    #raise SystemExit
 
-    # Get list of top-ranked stocks and store it in an array
-    # Calculate the mean returns and covariance of returns
+    portfolios_df = pd.DataFrame()
 
-    mean_historical_returns, covariance_of_returns = calculate_optimiser_inputs(stocks)
-    portfolio = optimise_portfolio_slsqp(mean_historical_returns, 
-                              covariance_of_returns,
-                              risk_free_rate=risk_free_rate,
-                              bounds=bounds,
-                              penalty=penalty)
+    gammas = list(range(0, 45))
+    lbs = np.linspace(0.000, 0.025, 7)
+    ubs = [0.08, 1]
+
+    for gamma, lb, ub in product(gammas, lbs, ubs):
+        o = Optimizer(mu, Sigma, bounds=(lb, ub), penalty=gamma)
+        params = {"sharpe":o.sharpe, "mean":o.mean, "sd":o.sd, 
+                "bounds":o.bounds, "penalty":o.penalty, "unique":np.sum(o.weights - o.weights.min() > 0.01)}
+        portfolios_df = portfolios_df.append(params, ignore_index=True)
+        #o.print_summary()
+    sorted_pf = portfolios_df.sort_values(by=["unique", "sharpe"],
+                                          ascending=False)
+    print(sorted_pf)                                    
+
     
-    print(np.round(portfolio, 3))
-    weights = portfolio/np.sum(portfolio)
-    # Percentage allocated to each stock
 
-    portfolio_sd = np.sqrt(weights @ covariance_of_returns.values @ weights * 252)
-    portfolio_ev = weights @ mean_historical_returns.values * 252
-    sharpe = (portfolio_ev - risk_free_rate)/portfolio_sd
-
-    print("Sharpe Ratio: {}".format(sharpe))
-    print("Mean Return: {}".format(portfolio_ev))
-    print("Standard Deviation: {}".format(portfolio_sd))
-
-    print("Finished in {} seconds".format(time.time() - start))
