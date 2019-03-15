a984348e70e2972936b2c1bc16a1179e7d8b55cd
tao-yu <taoxyu@gmail.com>
2019-03-15 20:58:29 +0000

    Add comments

--- src/scipy_optimizer.py
+++ src/scipy_optimizer.py
@@ -8,10 +8,7 @@ import sys
 import argparse
 
 
-def calculate_optimiser_inputs(db,
-                               num_stocks,
-                               rankings_table,
-                               prices_table):
+def calculate_optimiser_inputs(db, num_stocks, rankings_table, prices_table):
     """Returns two pandas DataFrames: mean (daily) returns for each
      stock in tickers and matrix of covariances between stocks' returns."""
 
@@ -30,6 +27,11 @@ def calculate_optimiser_inputs(db,
 
 def neg_sharpe(weights, mu, Sigma,
                penalty=0, risk_free_rate=0):
+    """
+    Calculates the sharpe ratio of a portfolio given a set of weights, 
+    expected returns, a covariance matrix and risk_free_rate
+    """
+
     portfolio_sd = np.sqrt(weights @ Sigma.values @ weights * 252)
     portfolio_ev = weights @ mu.values * 252
     sharpe = (portfolio_ev - risk_free_rate)/portfolio_sd
@@ -39,6 +41,17 @@ def neg_sharpe(weights, mu, Sigma,
 class Optimizer:
     def __init__(self, mu, Sigma, bounds=(0, 1),
                  penalty=0, risk_free_rate=0, warnings=False):
+        """
+        :param mu: Mean of returns
+        :param Sigma: Covariance matrix of returns
+        :param bounds: Min and max weights to be assigned to any individual 
+        stock. Defaults to 0 and 1
+        :param penalty: Increasing this results in fewer nonzero weights
+        :param risk_free_rate: Risk free rate to be used in sharpe ratio calculations
+        :param warnings: Whether to print a warning when the weights don't add
+        up to 1
+        """
+
         self._mu = mu
         self._Sigma = Sigma
         self._bounds = bounds
@@ -85,6 +98,11 @@ class Optimizer:
         return (self.mean - self._risk_free_rate) / self.sd
 
     def print_summary(self):
+        """Prints a summary of the optimized portfolio
+        Includes sharpe ratio, mean and standard deviation as well as the bounds 
+        and penalty used
+        Also prints the percentage allocated to each stock
+        """
         print(50 * "=")
         if np.abs(np.sum(self._weights) - 1) > 0.001:
             print(f"Weights add up to {np.sum(self._weights)}")
@@ -108,33 +126,54 @@ if __name__ == "__main__":
     )
 
     if len(sys.argv) > 1:
+        """
+        type
+        python 5 0.025 0.08 
+        in the command line to optimize a portfolio using bounds of (0.025, 0.08)
+        and a penalty of 5 and print the results.
+        If no command line arguments are specified, this code will not run
+        """
         parser = argparse.ArgumentParser()
         parser.add_argument("penalty", nargs="?", default=0, type=float)
         parser.add_argument("lower_bound", nargs="?", default=0, type=float)
         parser.add_argument("upper_bound", nargs="?", default=1, type=float)
         args = parser.parse_args()
 
-        o = Optimizer(mu, Sigma, (args.lower_bound, args.upper_bound), args.penalty)
+        o = Optimizer(mu, Sigma, (args.lower_bound, args.upper_bound), 
+                      args.penalty)
         o.print_summary()
         weights = o.weights
         print(f"{np.sum(weights - weights.min() < 0.001)} weights at min")
         raise SystemExit
 
     portfolios_df = pd.DataFrame()
+    # A dataframe containing info on all of the portfolios found
 
-    gammas = list(range(0, 45))
-    lbs = np.linspace(0.000, 0.025, 7)
-    ubs = [0.08, 1]
+    gammas = list(range(0, 35)) # Range of penalty values to loop through
+    lbs = np.linspace(0.02, 0.033, 10) # Range of lower bounds to loop through
+    ubs = np.linspace(0.033, 1, 10) # Range of upper bounds to loop through
 
     for gamma, lb, ub in product(gammas, lbs, ubs):
+        print(gamma, lb, ub, end="\r")
         o = Optimizer(mu, Sigma, bounds=(lb, ub), penalty=gamma)
         params = {"sharpe":o.sharpe, "mean":o.mean, "sd":o.sd, 
-                "bounds":o.bounds, "penalty":o.penalty, "unique":np.sum(o.weights - o.weights.min() > 0.01)}
+                  "bounds":o.bounds, "penalty":o.penalty, 
+                  "unique":np.sum(o.weights - o.weights.min() > 0.01)}
         portfolios_df = portfolios_df.append(params, ignore_index=True)
         #o.print_summary()
-    sorted_pf = portfolios_df.sort_values(by=["unique", "sharpe"],
+    print()
+    sorted_pf = portfolios_df.sort_values(by=["sharpe"],
                                           ascending=False)
+    # Portfolios ranked by sharpe ratio
     print(sorted_pf)                                    
 
+    sorted_pf2 = portfolios_df.sort_values(by=["unique", "sharpe"],
+                                          ascending=False)
+    # Portfolios ranked by how many values there are that aren't just the min 
+    # value
+    print(sorted_pf2)                                    
+
+    sorted_pf[['sharpe', 'unique', 'bounds', 'penalty']].to_csv("sharpe.csv")
+    sorted_pf2[['sharpe', 'unique', 'bounds', 'penalty']].to_csv("unique.csv")
     
 
